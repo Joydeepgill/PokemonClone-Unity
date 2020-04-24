74aae659d7374334e5bcecf81efcf353e5af7f3b
Joydeepgill <joyydeep.gill@gmail.com>
2020-04-24 00:46:20 -0400

    Added variable representing the player sprite.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{
+{  
+    public Sprite playerSprite; 
+
     // Start is called before the first frame update
     void Start()
     {
