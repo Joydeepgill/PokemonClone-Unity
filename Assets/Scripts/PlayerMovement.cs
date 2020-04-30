ffd7de3f6c0ac8b1d53e51dbab5bb7042a2964a1
Joydeepgill <joyydeep.gill@gmail.com>
2020-04-30 18:41:55 -0400

    Deleting old code.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,27 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {  
-    public Sprite playerSprite; 
+    //Variable representing the speed
+    public float speed = 6f; 
+    //enables physical behaivour to the sprite 
+    public RigidBody2D rigidBody; 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    Vector2 movement; 
+    //Called when registering user input
     void Update()
     {
-        //Get the user input (by the keys) 
-        //Detect when left arrow key is pressed down 
-        if(Input.GetKey(KeyCode.LeftArrow)) {
-            //Move left 
-        } else if (Input.GetKey(KeyCode.RightArrow)) {
-            //Move Right 
-        } else if (Input.GetKey(KeyCode.UpArrow)) {
-            //Move up
-        } else {
-            //Move down
-        }
-    }
+        
+    } 
 }
