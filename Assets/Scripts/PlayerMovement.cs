53c8304626600f4fbac0773bcbec597b048123d6
Joydeepgill <joyydeep.gill@gmail.com>
2020-04-30 22:40:02 -0400

    refactored some code.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,29 @@ public class PlayerMovement : MonoBehaviour
 {  
     //Variable representing the speed
     public float speed = 6f; 
-    //enables physical behaivour to the sprite 
-    public RigidBody2D rigidBody; 
 
-    Vector2 movement; 
     //Called when registering user input
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+            //Get the current position of current GameObject
+            Vector3 currPosition = this.transform.position; 
+            //Decrease x  
+            currPosition.x--; 
+            //Set the new position 
+            this.currPosition = currPosition; 
+        } else if (Input.GetKeyDown(KeyCode.RightArrow)){ 
+            Vector3 currPosition = this.transform.position; 
+            currPosition.x++; 
+            this.currPosition = currPosition; 
+        } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            Vector3 currPosition = this.transform.position; 
+            currPosition.y--; 
+            this.currPosition = currPosition; 
+        } else {
+            Vector3 currPosition = this.transform.position; 
+            currPosition.y++; 
+            this.currPosition = currPosition; 
+        }
     } 
 }
