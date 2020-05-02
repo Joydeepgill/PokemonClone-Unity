810f9742c0d57958d99b3cb040a2402e7d917d1a
Joydeepgill <joyydeep.gill@gmail.com>
2020-05-02 00:03:55 -0400

    Added if-else statements for changing player position.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,32 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{  
-    //Variable representing the speed
-    public float speed = 6f; 
-
-    //Called when registering user input
-    void Update()
+{
+    // Start is called before the first frame update
+    void Start()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow)) {
-            //Get the current position of current GameObject
-            Vector3 currPosition = this.transform.position; 
-            //Decrease x  
-            currPosition.x--; 
-            //Set the new position 
-            this.currPosition = currPosition; 
-        } else if (Input.GetKeyDown(KeyCode.RightArrow)){ 
-            Vector3 currPosition = this.transform.position; 
-            currPosition.x++; 
-            this.currPosition = currPosition; 
-        } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            Vector3 currPosition = this.transform.position; 
-            currPosition.y--; 
-            this.currPosition = currPosition; 
-        } else {
-            Vector3 currPosition = this.transform.position; 
-            currPosition.y++; 
-            this.currPosition = currPosition; 
-        }
-    } 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        //Get current position of the player 
+        Vector3 position = transform.position; 
+
+        //Control player movement 
+        if(Input.GetKey(KeyCode.A)) {
+            //Get current player movement 
+            position.x = position.x--; 
+            transform.position = position; 
+            //KeyCode maps to a physical bkey on the keyboard 
+        } else if (Input.GetKey(KeyCode.D)) {
+            position.x++; 
+        } else if (Input.GetKey(KeyCode.W)) {
+            position.y++; 
+        } else if (Input.GetKey(KeyCode.S)) {
+            position.y--; 
+        } 
+    }
 }
