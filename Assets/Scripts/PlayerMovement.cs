e550122e4638e25a1df456e82cff71c827e81308
Joydeepgill <joyydeep.gill@gmail.com>
2020-05-02 21:36:07 -0400

    Trying to debug things in the update() function.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,26 +7,25 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
-    { 
+    {  
+        Debug.Log("update the player movement!");
         //Get current position of the player 
         Vector3 position = transform.position; 
-
         //Control player movement 
-        if(Input.GetKey(KeyCode.A)) {
+        if(Input.GetKey(KeyCode.LeftArrow)) {
             //Get current player movement 
             position.x = position.x--; 
             transform.position = position; 
             //KeyCode maps to a physical bkey on the keyboard 
-        } else if (Input.GetKey(KeyCode.D)) {
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
             position.x++; 
-        } else if (Input.GetKey(KeyCode.W)) {
+        } else if (Input.GetKey(KeyCode.UpArrow)) {
             position.y++; 
-        } else if (Input.GetKey(KeyCode.S)) {
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
             position.y--; 
         } 
     }
