e98bae0e636b7fc174f8aea9b3bb07339f5ee9a3
Joydeepgill <joyydeep.gill@gmail.com>
2020-05-03 00:12:40 -0400

    Completed basic player movement.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,30 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{
+{ 
+    Vector3 tempPosition; 
     // Start is called before the first frame update
     void Start()
     {
+        tempPosition = transform.position; 
     }
 
     // Update is called once per frame
     void Update()
     {  
-        Debug.Log("update the player movement!");
-        //Get current position of the player 
-        Vector3 position = transform.position; 
         //Control player movement 
         if(Input.GetKey(KeyCode.LeftArrow)) {
             //Get current player movement 
-            position.x = position.x--; 
-            transform.position = position; 
+            tempPosition.x -= 1f; 
+            transform.position = tempPosition; 
             //KeyCode maps to a physical bkey on the keyboard 
         } else if (Input.GetKey(KeyCode.RightArrow)) {
-            position.x++; 
+            tempPosition.x += 1f; 
+            transform.position = tempPosition; 
         } else if (Input.GetKey(KeyCode.UpArrow)) {
-            position.y++; 
+            tempPosition.y += 1f; 
+            transform.position = tempPosition; 
         } else if (Input.GetKey(KeyCode.DownArrow)) {
-            position.y--; 
+            tempPosition.y -= 1f; 
+            transform.position = tempPosition; 
         } 
     }
 }
