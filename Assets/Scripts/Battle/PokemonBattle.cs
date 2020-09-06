7bf499a050a33e834a5a0450f9f9ff2993355ab0
Joydeepgill <joyydeep.gill@gmail.com>
2020-09-05 23:54:10 -0400

    Adding a class to represent/simulate pokemon battles.

---
+++ Assets/Scripts/Battle/PokemonBattle.cs
@@ -0,0 +1,10 @@
+public class PokemonBattle : Monobehaivour
+{ 
+    //Returns true if a player is able to use available 
+    public bool useMedicine(Dictionary<string, int> medicine)
+    {
+        bool use = false; 
+        
+        //TODO: player can only use item if amount > 1 in medicine bag 
+    }
+}
