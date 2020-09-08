ce081c671c0a2e5bcb00864e72d5938686c9d26e
Joydeepgill <joyydeep.gill@gmail.com>
2020-09-08 15:56:40 -0400

    Added the base stats for Bulbasaur.

--- Assets/Scripts/Battle/PokemonBattle.cs
+++ Assets/Scripts/Battle/PokemonBattle.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public class PokemonBattle : Monobehaivour
 { 
     PlayerInventory medication = new PlayerInventory();  
-    Pokemon bulbasaur = new Pokemon(); 
-
+    Pokemon bulbasaur = new Pokemon(49, 45, 65, 65, 45); 
     Pokemon trainerSquirtle = new Pokemon(); 
 
     //Returns true if a player is able to use available items 
@@ -19,7 +18,9 @@ public class PokemonBattle : Monobehaivour
     }
 
     //TODO: Calculate the pokemon damamge depending on the type of attack 
+    //Pass in attack type
     public void calculateDamage() {
+        //Depends on attack type 
 
     }
 }
