4c1c46d7320eaba1fef71ce39bc171641ab41a24
Joydeepgill <joyydeep.gill@gmail.com>
2020-10-07 21:43:04 -0400

    Initalize an empty player inventory diciontary in constructor.

--- Assets/Scripts/Tests.cs
+++ Assets/Scripts/Tests.cs
@@ -3,6 +3,16 @@ using UnityEngine;
 public class Tests 
 {
 	void Start () {
-		
+
+	} 
+
+	//Tests concerning the PlayerInventory class
+	void TestPlayerInventory () {
+		var bag = new Dictionary<srting, Dictionary<string, int>>{
+        {"Medicine", new Dictionary<string, int>{{"Burn Heal", 1}, {"Potion", 10}, {"Heal Powder", 1}}}, 
+        {"PokeBall", new Dictionary<string, int>{{"Pokeball", 15}, {"Friend Ball", 20}, {"Ultra Ball", 20}}}
+    	};    
+
+    	
 	}
 }
