8655c22beb60b56f795e6f80e5b18fe3a909f1f7
Joydeep Gill <joydeepgill@Joydeeps-MacBook-Air.local>
2020-10-18 00:54:30 -0400

    Reorganizingfiles

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{ 
+    Vector3 tempPosition; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        tempPosition = transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        //Control player movement 
+        if(Input.GetKey(KeyCode.LeftArrow)) {
+            //Get current player movement 
+            tempPosition.x -= 1f; 
+            transform.position = tempPosition; 
+            //KeyCode maps to a physical bkey on the keyboard 
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            tempPosition.x += 1f; 
+            transform.position = tempPosition; 
+        } else if (Input.GetKey(KeyCode.UpArrow)) {
+            tempPosition.y += 1f; 
+            transform.position = tempPosition; 
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
+            tempPosition.y -= 1f; 
+            transform.position = tempPosition; 
+        } 
+    }
+}
